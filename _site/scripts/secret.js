667f1aa78daa077da8461582666e274032958950
DestinyPlayer <thenextdecade244@gmail.com>
2024-01-13 17:53:42 +0300

    Removed the _site from .gitignore

---
+++ _site/scripts/secret.js
@@ -0,0 +1,28 @@
+function secret() {
+  const secret01 = document.getElementById("secretone");
+  const secret02 = document.getElementById("secrettwo");
+  
+  const date = new Date();
+  var day = date.getDay();
+  
+  const secretlist1 = [
+    "Velkam to mai devilish leir",
+    "Sethorven iz oosam",
+    "Kobalstromo iz adorabl",
+    "Brutali efishent self",
+    "Bip bip aim ae ship",
+    "E snoumaen kilo",
+    "Efs in chat fo"
+  ];
+  const secretlist2 = [
+    "Houp iu enjoi ior stei",
+    "Pliz voch him",
+    "Bat dont tel him ai seid zaet",
+    "Replikeiting Mekanism ov vor",
+    "Fak fak aim ae dak",
+    "Dai tinz ae parlis",
+    "Ol ze pur bastads"
+  ];
+  secret01.innerHTML = secretlist1[day];
+  secret02.innerHTML = secretlist2[day];
+}
