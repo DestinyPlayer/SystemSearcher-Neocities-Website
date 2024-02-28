10429eb8ffa246455aff2a4877e554c44f8b11cf
DestinyPlayer <thenextdecade244@gmail.com>
2024-02-28 04:27:36 +0300

    Test

--- _site/scripts/readImages.js
+++ _site/scripts/readImages.js
@@ -1,4 +1,9 @@
+const filler1 = '<div class="image-button" style="width:10%;">\n    <img src="'
+const filler2 = ' class="image-base" style="border-radius: 25px;">\n</div>'
 function readImages(path) {
-    var mostRecent = document.getElementById("mostRecent");
-    mostRecent["src"] = path;
+    target = document.getElementById("fillImages")
+    for (let i = 0; i < images.length; i++) {
+        let text = filler1 + images[i] + filler2;
+        target.innerHTML = text;
+    }
 }
