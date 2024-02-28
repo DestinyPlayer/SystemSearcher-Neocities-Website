5a03ab5145ee9ac7d2395252eb481df3ac884159
DestinyPlayer <thenextdecade244@gmail.com>
2024-02-28 04:36:49 +0300

    Update readImages.js

--- _site/scripts/readImages.js
+++ _site/scripts/readImages.js
@@ -1,9 +1,9 @@
 const filler1 = '<div class="image-button" style="width:10%;">\n    <img src="'
-const filler2 = ' class="image-base" style="border-radius: 25px;">\n</div>'
+const filler2 = '" class="image-base" style="border-radius: 25px;">\n</div>\n\n'
 function readImages() {
     target = document.getElementById("fillImages")
     for (let i = 0; i < images.length; i++) {
-        let text = filler1 + images[i] + filler2;
+        let text = text + (filler1 + images[i] + filler2);
         target.innerHTML = text;
     }
 }
