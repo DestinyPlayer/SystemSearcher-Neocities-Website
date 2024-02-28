cc2a3d5cc56f4652bcee6e2e7155e48373ea3497
DestinyPlayer <thenextdecade244@gmail.com>
2024-02-28 04:52:19 +0300

    Update readImages.js

--- _site/scripts/readImages.js
+++ _site/scripts/readImages.js
@@ -1,12 +1,12 @@
-const filler = '<div class="image-button" style="width:40%;">\n<img src=[IMAGEHERE] class="image-base">\n<img src=[IMAGEHERE] class="image-content">\n</div>\n\n'
+const filler = '<div class="image-button" style="width:40%;">\n<img src="IMAGEHERE" class="image-base">\n<img src="IMAGEHERE" class="image-content">\n</div>\n\n'
 
 var textFill = ""
 function readImages() {
     target = document.getElementById("fillImages")
     for (let i = 0; i < images.length; i++) {
-        textFill = textFill + filler.replace(/[IMAGEHERE]/g, images[i]);
-        target.innerHTML = textFill;
+        textFill = textFill + filler.replace(/IMAGEHERE/g, images[i]);
     }
+    target.innerHTML = textFill;
 }
 
 //<div class="image-button" style="width:40%;">
